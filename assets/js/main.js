3de17099ad31b2d68527ed9f4a7d99250ba9d0c5
Justin Blacksher <j.redwatersec@protonmail.ch>
2025-11-28 17:43:39 -0700

    added content

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// Auto-update year
+document.addEventListener("DOMContentLoaded", () => {
+  const yearEl = document.getElementById("year");
+  if (yearEl) yearEl.textContent = new Date().getFullYear();
+});
